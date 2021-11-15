ec1b04d7ab1f9179dd6abb8b46c1546c0790f1fe
Ashwin1234 <ashwinsrivatsab@gmail.com>
2021-11-15 03:34:49 -0600

    created Rest apis

--- Implementation/backend - flask/app.py
+++ Implementation/backend - flask/app.py
@@ -1,6 +1,40 @@
+import json
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello, Flask!"
+#get all sensors data
+@app.route('/sensors',methods=['GET'])
+def get_sensors():
+    return json.dumps({})
+
+#get all mismtaches of all sensors
+@app.route('/sensors_mismatch',methods=['GET'])
+def get_sensors_mismatch():
+    return json.dumps({})
+
+#get mismtach of particular sensor based on id
+@app.route('/particular_mismatch/<id>',methods=['GET'])
+def get_particular_mismatch(id):
+    return json.dumps({})
+
+#get matches of particular sensor based on id 
+@app.route('/particular_match/<id>',methods=['GET'])
+def get_particular_match(id):
+    return json.dumps({})
+
+#get matches of particular sensor based on id 
+@app.route('/particular_match/<id>',methods=['GET'])
+def get_particular_match(id):
+    return json.dumps({})
+
+#get mismatches based on a particular region 
+@app.route('/location_mismatch',methods=['GET'])
+def get_particular_match():
+    return json.dumps({})
+
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True, host='127.0.0.1', port=8080)
+
